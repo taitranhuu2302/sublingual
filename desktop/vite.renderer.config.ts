08284c0aaab0a13840bf39296f6ece17240b8547
taitranhuu2302 <taitranhuu2002@gmail.com>
2026-05-04 16:36:59 +0700

    feat(desktop): add shadcn

--- desktop/vite.renderer.config.ts
+++ desktop/vite.renderer.config.ts
@@ -1,4 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import path from "node:path";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig(async () => {
+  const { default: tailwindcss } = await import("@tailwindcss/vite");
+  return {
+    plugins: [tailwindcss()],
+    publicDir: path.resolve(__dirname, "assets"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/renderer/src"),
+      },
+    },
+  };
+});
