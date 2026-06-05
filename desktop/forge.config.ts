0a3d536ae02924b915d810a4fb42e38cf3054eb9
taitranhuu2302 <tai.tranhuu2002@gmail.com>
2026-06-06 00:03:20 +0700

    fix: remove MakerSquirrel to allow Windows cross-compile ZIP on macOS

--- desktop/forge.config.ts
+++ desktop/forge.config.ts
@@ -1,5 +1,4 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
@@ -20,7 +19,6 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({}),
     new MakerZIP({}, ['darwin', 'win32']),
     new MakerRpm({}),
     new MakerDeb({}),
